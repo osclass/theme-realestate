3a47c834afa425acdd6645fbdb851c07de33ec28
conejoninja <conejo@theninjabunny.com>
2013-10-22 11:45:35 +0200

    first commit at new repo

---
+++ realestate/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Theme Name: Osclass realestate Theme
+Theme URI: http://osclass.org
+Description: This is the Osclass realestate theme
+Version: 1.0.9
+Author:Osclass team
+Author URI: http://osclass.org
+Widgets: header,footer
+Theme update URI: realestate
+*/
+
+    function realestate_theme_info() {
+        return array(
+             'name'        => 'Osclass realestate Theme'
+            ,'version'     => '1.0.9'
+            ,'description' => 'This is the Osclass realestate theme'
+            ,'author_name' => 'Osclass Team'
+            ,'author_url'  => 'http://osclass.org'
+            ,'locations'   => array('header', 'footer')
+        );
+    }
+
+?>
